b9b9f6504074b47035fcccc35b7dc961270d4bb1
Taribi-Isaac <taribiforyou@gmail.com>
2023-01-09 02:25:09 +0100

    part of the first commit

---
+++ web-rsl/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCloULT8oyjtgnEeVcMOk4zmqU9OsvCKso",
+  authDomain: "web-rslhulls.firebaseapp.com",
+  projectId: "web-rslhulls",
+  storageBucket: "web-rslhulls.appspot.com",
+  messagingSenderId: "830000413578",
+  appId: "1:830000413578:web:674cb91f6f61057f53a673",
+  measurementId: "G-V01KTNQGVJ"
+};
