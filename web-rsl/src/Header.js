ceb898f38f8f3adebf2f56593f30f9f9d7338459
Taribi-Isaac <taribiforyou@gmail.com>
2023-01-09 02:24:23 +0100

    first commit

---
+++ web-rsl/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <img className='header_logo' src='./assets/logo.png'/>
+      
+
+    </div>
+  )
+}
+
+export default Header
